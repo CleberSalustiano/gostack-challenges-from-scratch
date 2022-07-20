32e6a5ffb12c88d59b360851fa3f4662c66b6b75
clebersalustiano <kleberdejesus45@hotmail.com>
2022-07-20 09:04:01 -0300

    feat: challenge04 template created

---
+++ challenges/challenge04/src/routes/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const ApplicationRoutes: React.FC = () => (
+  <Routes>
+    <Route path="/" element={/*page*/} />
+  </Routes>
+);
+
+export default ApplicationRoutes;
